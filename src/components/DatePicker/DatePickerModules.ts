ac82f7721f781773aef814c7571fdaf39b864c67
Bryndin Alexander <alexanderbrendin@gmail.com>
2023-04-06 21:46:48 +0300

    Fixed

--- src/components/DatePicker/DatePickerModules.ts
+++
@@ -1,8 +0,0 @@
-export * from './handleTheme';
-export * from './handleLang';
-export * from './handleMonth';
-export * from './handleWindow';
-export * from './handlerDate';
-export * from './handleReserve';
-export * from './generateCalendar';
-
